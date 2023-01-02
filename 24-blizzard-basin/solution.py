8dbfeccb4e54d667ca9a56f8f9ceb355d1b5d61f
johntelforduk <jtelford1@gmail.com>
2023-01-02 22:09:16 +0000

    Day 24, better search.

--- 24-blizzard-basin/solution.py
+++ 24-blizzard-basin/solution.py
@@ -1,50 +1,47 @@
 # Solution to day 24 of AOC 2022,
 # https://adventofcode.com/2022/day/24
 
-import copy
-import random
+import sys
 
+# Key = Minute integer.
+# Value = Dictionary, Keys = coordinates of 1 or more blizzards, Value = String of blizzard directions.
+BLIZZARD_MINS = {}
 
-class Basin:
+RIGHT, BOTTOM = 0, 0            # Edges of the basin.
+LINES = []                      # Used for rendering of the basin.
+HOME = (1, 0)
+GOAL = (0, 0)                   # Will be populated with the coordinates of the goal.
 
-    def __init__(self, raw: str):
+def setup(raw: str):
+    print('Pre-calculating blizzard maps.')
 
-        # Key = (x, y) current coordinates of 1 or more blizzards.
-        # Value = string of blizzard direction of travel.
-        self.blizzards = {}
+    global LINES, RIGHT, BOTTOM, BLIZZARD_MINS, GOAL
 
-        self.lines = raw.split('\n')
-        x, y = 0, 0
-        for row in self.lines:
-            x = 0
-            for c in row:
-                if c in '<>^v':
-                    self.blizzards[x, y] = c
-                x += 1
-            y += 1
+    # Key = (x, y) current coordinates of 1 or more blizzards.
+    # Value = string of blizzard direction of travel.
+    blizzards = {}
 
-        self.right, self.bottom = x - 2, y - 2
+    LINES = raw.split('\n')
+    x, y = 0, 0
+    for row in LINES:
+        x = 0
+        for c in row:
+            if c in '<>^v':
+                blizzards[x, y] = c
+            x += 1
+        y += 1
 
-        self.expedition = (1, 0)        # "Your expedition begins in the only non-wall position in the top row..."
-        self.goal = (x - 2, y - 1)      # "... and needs to reach the only non-wall position in the bottom row."
-        self.minute = 0
+    RIGHT, BOTTOM = x - 2, y - 2
 
-    def hash_state(self):
-        return self.expedition
+    # self.expedition = (1, 0)        # "Your expedition begins in the only non-wall position in the top row..."
+    GOAL = (x - 2, y - 1)      # "... and needs to reach the only non-wall position in the bottom row."
 
+    deltas = {'<': (-1, 0), '>': (1, 0), '^': (0, -1), 'v': (0, 1)}
+    for minute in range(350):
+        BLIZZARD_MINS[minute] = blizzards
 
-    def new_minute(self):
-        global PREVIOUS_BLIZZARDS
-
-        self.minute += 1
-        if self.minute in PREVIOUS_BLIZZARDS:
-            self.blizzards = PREVIOUS_BLIZZARDS[self.minute]
-            return
-
-        old_blizzards = self.blizzards.copy()
-
-        deltas = {'<': (-1, 0), '>': (1, 0), '^': (0, -1), 'v': (0, 1)}
-        self.blizzards = {}
+        old_blizzards = blizzards.copy()
+        blizzards = {}
 
         for x, y in old_blizzards:
             for direction in old_blizzards[(x, y)]:
@@ -52,133 +49,196 @@ class Basin:
                 new_x, new_y = x + dx, y + dy
 
                 # If blizzard goes off edge of basin, flip around.
-                if new_x > self.right:
+                if new_x > RIGHT:
                     new_x = 1
                 if new_x == 0:
-                    new_x = self.right
-                if new_y > self.bottom:
+                    new_x = RIGHT
+                if new_y > BOTTOM:
                     new_y = 1
                 if new_y == 0:
-                    new_y = self.bottom
+                    new_y = BOTTOM
 
-                if (new_x, new_y) in self.blizzards:
-                    self.blizzards[(new_x, new_y)] += direction
+                if (new_x, new_y) in blizzards:
+                    blizzards[(new_x, new_y)] += direction
                 else:
-                    self.blizzards[(new_x, new_y)] = direction
+                    blizzards[(new_x, new_y)] = direction
+    print('Blizzard maps calculated.')
 
-        PREVIOUS_BLIZZARDS[self.minute] = self.blizzards.copy()
 
+def render(exp_x: int, exp_y: int, minute: int):
+    print('\nminute:', minute)
+    x, y = 0, 0
+    for row in LINES:
+        for c in row:
+            if c in '<>^v':
+                c = '.'
+            if (exp_x, exp_y) == (x, y):
+                c = 'E'
+            elif GOAL == (x, y):
+                c = 'G'
+            else:
+                if (x, y) in BLIZZARD_MINS[minute]:
+                    blizz_str = BLIZZARD_MINS[minute][(x, y)]
+                    if len(blizz_str) == 1:
+                        c = blizz_str
+                    else:
+                        c = str(len(blizz_str))
 
-    def render(self):
-        print('minute:', self.minute)
-        x, y = 0, 0
-        for row in self.lines:
-            for c in row:
-                if c in '<>^v':
-                    c = '.'
-                if self.expedition == (x, y):
-                    c = 'E'
-                elif self.goal == (x, y):
-                    c = 'G'
-                else:
-                    if (x, y) in self.blizzards:
-                        blizz_str = self.blizzards[(x, y)]
-                        if len(blizz_str) == 1:
-                            c = blizz_str
-                        else:
-                            c = str(len(blizz_str))
-
-                print(c, end='')
-                x += 1
-            print()
-            x = 0
-            y += 1
+            print(c, end='')
+            x += 1
         print()
-
-    def manhattan(self):
-        x, y = self.expedition
-        return x + y
-
-    def possible_moves(self) -> list:
-        """Return a list of possible (x, y) coordinates that expedition could move to next."""
-        x, y = self.expedition
-        possible = [(x, y)]                                 # "...or you can wait in place."
-        for dx, dy in [(1, 0), (0, 1), (0, -1), (0, -1)]:   # "On each minute, you can move up, down, left, or right..."
-            px, py = x + dx, y + dy
-            if (px, py) == self.goal:
-                return[self.goal]
-
-            if (px, py) not in self.blizzards and 1 <= px <= self.right and 1 <= py <= self.bottom:
-                possible.append((x + dx, y + dy))
-
-        random.shuffle(possible)
-        return possible
+        x = 0
+        y += 1
 
 
-def search(this_basin: Basin):
-    global LEAST_MINUTES
-    global PREVIOUS_STATES
+def possible_moves(exp_x: int, exp_y: int, minute: int) -> list:
+    """Return a list of possible (x, y) coordinates that expedition could move to next."""
+    global HOME
+    possible = []                                               # "...or you can wait in place."
+    for dx, dy in [(0, 0), (1, 0), (0, 1), (0, -1), (0, -1)]:   # "On each minute, you can move up, down, left, or right..."
+        px, py = exp_x + dx, exp_y + dy
+        if (px, py) == GOAL:                            # Moving to goal is possible, so no other moves needed.
+            return[(px, py, minute)]
 
-    # print(LEAST_MINUTES, this_basin.minute, this_basin.expedition)
+        if (px, py) == HOME or ((px, py) not in BLIZZARD_MINS[minute] and 1 <= px <= RIGHT and 1 <= py <= BOTTOM):
+            possible.append((px, py, minute))
 
-    if this_basin.minute + 1 >= LEAST_MINUTES:      # We've already found a quicker route, so give up.
-        return
-
-    if this_basin.minute > 900:                 # Search too long, so give up.
-        return
-
-    if this_basin.minute > 3.07 * this_basin.manhattan():
-        return
-
-    # hash = this_basin.hash_state()
-    # if hash in PREVIOUS_STATES:
-    #     if PREVIOUS_STATES[hash] <= this_basin.minute:
-    #         return
-    #     PREVIOUS_STATES[hash] = this_basin.minute
-
-    this_basin.new_minute()                     # Move the blizzards, and add 1 to the minute count.
+    return possible
 
 
-    # print(this_basin.hash_state())
-
-    if this_basin.expedition == this_basin.goal:
-        LEAST_MINUTES = this_basin.minute
-        print(LEAST_MINUTES)
-        return
-
-    for possible_expedition in this_basin.possible_moves():
-        new_basin = copy.deepcopy(this_basin)
-        new_basin.expedition = possible_expedition
-        search(this_basin=new_basin)
+def count_non_maxsize(q: dict) -> int:
+    num = 0
+    for x, y, m in q:
+        if q[(x, y, m)] != sys.maxsize:
+            num += 1
+    return num
 
 
 f = open('input.txt')
 t = f.read()
 f.close()
 
-basin = Basin(raw=t)
-# print(basin.blizzards)
-# print(basin.expedition, basin.goal)
-basin.render()
+setup(raw=t)
+# render(exp_x=1, exp_y=0, minute=0)
+# print(possible_moves(1, 0, 0))
+#
+# render(exp_x=1, exp_y=0, minute=1)
+# print(possible_moves(1, 0, 1))
+#
+# render(exp_x=1, exp_y=0, minute=2)
+# render(exp_x=1, exp_y=0, minute=3)
+# render(exp_x=1, exp_y=0, minute=4)
+# render(exp_x=1, exp_y=0, minute=5)
 
-LEAST_MINUTES = 9999999
-PREVIOUS_STATES = {}
-PREVIOUS_BLIZZARDS = {}
+# Implemented pseudo code from, https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+# function Dijkstra(Graph, source):
+#
+# create vertex set Q
+q, dist, prev = {}, {}, {}
+# for each vertex v in Graph:
 
-search(this_basin=basin)
+# for v in grid:
+for minute in range(0, 350):
+    for sx, sy in [HOME, GOAL]:
+        dist[sx, sy, minute] = sys.maxsize
+        prev[sx, sy, minute] = None
+        q[sx, sy, minute] = dist[sx, sy, minute]
 
-print('Part 1:', LEAST_MINUTES)
-# basin.new_minute()
-# basin.render()
-#
-# basin.new_minute()
-# basin.render()
-#
-# basin.new_minute()
-# basin.render()
-#
-# basin.new_minute()
-# basin.render()
-#
-# basin.new_minute()
-# basin.render()
+    for y in range(1, BOTTOM + 1):
+        for x in range(1, RIGHT + 1):
+            # dist[v] ← INFINITY
+            dist[(x, y, minute)] = sys.maxsize
+            # prev[v] ← UNDEFINED
+            prev[(x, y, minute)] = None
+            # add v to Q
+            q[(x, y, minute)] = dist[(x, y, minute)]
+
+# Add the GOAL location specifically, as it is outside of the nested loops above.
+
+hx, hy = HOME
+SOURCE = (hx, hy, 0)
+# dist[source] ← 0
+dist[SOURCE] = 0
+q[SOURCE] = 0
+
+# print(q)
+
+lowest = sys.maxsize
+
+# while Q is not empty:
+done = False
+while not done:
+
+    len_q = len(q)
+    if len_q % 10 == 0:
+        print('len(q):', len(q), count_non_maxsize(q), lowest)
+
+    # u ← vertex in Q with min dist[u]
+    u = min(q, key=q.get)
+
+    if q[u] == sys.maxsize:
+        done = True
+
+    else:
+        # print('u, q[u], dist[u]:', u, q[u], dist[u])
+
+        # remove u from Q
+        del q[u]
+
+        # for each neighbor v of u still in Q:
+        x, y, minute = u
+
+        if minute < lowest:
+
+            if (x, y) == GOAL:
+                lowest = min(lowest, minute)
+
+            # print(x, y)
+            minute += 1
+
+            # render(exp_x=x, exp_y=y, minute=minute)
+
+            if minute < 350:
+                for v in possible_moves(exp_x=x, exp_y=y, minute=minute):
+                    # for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    #     v = (x + dx, y + dy)
+                    #     climbable = False
+                    #     if v in grid:
+                    #         elevation = ord(grid[v]) - ord(grid[u])
+                    #         climbable = (elevation <= 1)
+                    #         # print(v, elevation)
+                    #
+                    #     # TODO v in q yes... but also, is there a path from u to v.
+                    #     if v in q and climbable:
+                            # print('v:', v)
+
+                            # print(v)
+                            # alt ← dist[u] + length(u, v)
+                            # alt = dist[u] + grid[v]
+                            # XXX All edges are length one in this graph!!!
+                            alt = minute
+
+                            # if alt < dist[v]:
+                            if alt < dist[v]:
+                                # dist[v] ← alt
+                                dist[v] = alt
+                                if v in q:
+                                    q[v] = alt
+                                # prev[v] ← u
+                                prev[v] = u
+
+# return dist[], prev[]
+# print(dist[GOAL])
+
+
+# print(dist)
+
+minute, part1 = 0, None
+gx, gy = GOAL
+while part1 is None:
+    d = dist[(gx, gy, minute)]
+    if d != sys.maxsize:
+        part1 = d
+    minute += 1
+
+print('Part1:', part1)
